06a35a5bce61e9af9807905b5edb993975bb529a
samcarter <sam.carter8@aol.com>
2020-01-02 20:52:13 +0100

    experimenting with automatic commits for l3build tag

--- build.lua
+++ build.lua
@@ -2,12 +2,21 @@
 
 -- setting up the name
 module = "tikzducks"
-packageversion="1.2"
+packageversion="1.3"
 packagedate = "2020-01-02"
 
--- documentation
-typesetfiles = {"tikzducks-doc.tex"}
-docfiles     = {"tikzducks-doc.tex"}
+function git(...)
+    local args = {...}
+    table.insert(args, 1, 'git')
+    local cmd = table.concat(args, '')
+    print('Executing:', cmd)
+    os.execute(cmd)
+end
+
+function sleep(s)
+  local ntime = os.clock() + s/10
+  repeat until os.clock() > ntime
+end
 
 -- Auto-versioning ---------------------------------------------------
 tagfiles = {"tikzducks.sty", "tikzducks-doc.tex"}
@@ -19,14 +28,21 @@ function update_tag (file,content,tagname,tagdate)
 			"\\ProvidesPackage{(.-)}%[%d%d%d%d%/%d%d%/%d%d version v%d%.%d",
 			"\\ProvidesPackage{%1}[" .. tagdate.." version v"..packageversion
 		)
+		git(" add ", file)
 		return content
 	elseif string.match (file, "*-doc.tex$" ) then
 		content = string.gsub (
 			content,
-			"\\date{Version v%d%.%d",
-			"\\date{Version v" .. packageversion
+			"\\date{Version v%d%.%d \\textendash{} %d%d%d%d%/%d%d%/%d%d",
+			"\\date{Version v" .. packageversion .. " \\textendash{} " .. tagdate
 		)
+		git(" add ", file)
+		os.execute("sleep 1 ; latexmk " .. module .. "-doc")
+		os.execute("cp tikzducks-doc.pdf documentation.pdf")
+		git(" add ", "documentation.pdf")
+		git(" commit -m 'step tag ", packageversion, "'" )
+		git(" tag ", "v", packageversion)
 		return content
 	end
- return content
+	return content
 end
