9387824e974dc9f901ba349b89421ce38566c88c
samcarter <sam.carter8@aol.com>
2020-01-02 17:55:41 +0100

    experimenting with l3build

---
+++ build.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env texlua
+
+-- setting up the name
+module = "tikzducks"
+packageversion="1.2"
+packagedate = "2020-01-02"
+
+-- documentation
+typesetfiles = {"tikzducks-doc.tex"}
+docfiles     = {"tikzducks-doc.tex"}
+
+-- Auto-versioning ---------------------------------------------------
+tagfiles = {"tikzducks.sty"}
+function update_tag (file,content,tagname,tagdate)
+ tagdate = string.gsub (packagedate,"-", "/")
+ if string.match (file, "%.sty$" ) then
+  content = string.gsub (content,  
+                         "\\ProvidesPackage{(.-)}%[%d%d%d%d%/%d%d%/%d%d version v%d%.%d",
+                         "\\ProvidesPackage{%1}[" .. tagdate.." version v"..packageversion)                       
+  return content  
+ end
+ return content
+end
