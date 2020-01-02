472194a587146aa5581a7e838df8faa242d23234
samcarter <sam.carter8@aol.com>
2020-01-02 21:26:24 +0100

    experimenting

--- build.lua
+++ build.lua
@@ -2,21 +2,16 @@
 
 -- setting up the name
 module = "tikzducks"
-packageversion="1.3"
+packageversion="v1.3"
 packagedate = "2020-01-02"
 
 -- Auto-versioning ---------------------------------------------------
 function git(...)
     local args = {...}
     table.insert(args, 1, 'git')
-    local cmd = table.concat(args, '')
+    local cmd = table.concat(args, ' ')
     print('Executing:', cmd)
     os.execute(cmd)
-end
-
-function sleep(s)
-  local ntime = os.clock() + s/10
-  repeat until os.clock() > ntime
 end
 
 tagfiles = {"*.sty", "tikzducks-doc.tex"}
@@ -26,14 +21,14 @@ function update_tag (file,content,tagname,tagdate)
 		content = string.gsub (
 			content,
 			"\\ProvidesPackage{(.-)}%[%d%d%d%d%/%d%d%/%d%d version v%d%.%d",
-			"\\ProvidesPackage{%1}[" .. tagdate.." version v"..packageversion
+			"\\ProvidesPackage{%1}[" .. tagdate.." version "..packageversion
 		)
 		return content
 	elseif string.match (file, "*-doc.tex$" ) then
 		content = string.gsub (
 			content,
 			"\\date{Version v%d%.%d \\textendash{} %d%d%d%d%/%d%d%/%d%d",
-			"\\date{Version v" .. packageversion .. " \\textendash{} " .. tagdate
+			"\\date{Version " .. packageversion .. " \\textendash{} " .. tagdate
 		)
 		return content
 	end
@@ -41,12 +36,11 @@ function update_tag (file,content,tagname,tagdate)
 end
 
 function tag_hook(tagname)
-	git(" add *.sty")
-	git(" add *-doc.tex")
-	sleep(1)
+	git("add", "*.sty")
+	git("add", "*-doc.tex")
 	os.execute("latexmk " .. module .. "-doc")
 	os.execute("cp tikzducks-doc.pdf documentation.pdf")
-	git(" add ", "documentation.pdf")
-	git(" commit -m 'step tag ", packageversion, "'" )
-	git(" tag ", "v", packageversion)
+	git("add", "documentation.pdf")
+	git("commit -m 'step tag ", packageversion, "'" )
+	git("tag", packageversion)
 end
