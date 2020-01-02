156332e4f3142b2cdcf6932851aa4a86297ff328
samcarter <sam.carter8@aol.com>
2020-01-02 18:15:22 +0100

    adding documentation to l3build autotag

--- build.lua
+++ build.lua
@@ -10,14 +10,23 @@ typesetfiles = {"tikzducks-doc.tex"}
 docfiles     = {"tikzducks-doc.tex"}
 
 -- Auto-versioning ---------------------------------------------------
-tagfiles = {"tikzducks.sty"}
+tagfiles = {"tikzducks.sty", "tikzducks-doc.tex"}
 function update_tag (file,content,tagname,tagdate)
- tagdate = string.gsub (packagedate,"-", "/")
- if string.match (file, "%.sty$" ) then
-  content = string.gsub (content,  
-                         "\\ProvidesPackage{(.-)}%[%d%d%d%d%/%d%d%/%d%d version v%d%.%d",
-                         "\\ProvidesPackage{%1}[" .. tagdate.." version v"..packageversion)                       
-  return content  
- end
+	tagdate = string.gsub (packagedate,"-", "/")
+	if string.match (file, "%.sty$" ) then
+		content = string.gsub (
+			content,
+			"\\ProvidesPackage{(.-)}%[%d%d%d%d%/%d%d%/%d%d version v%d%.%d",
+			"\\ProvidesPackage{%1}[" .. tagdate.." version v"..packageversion
+		)
+		return content
+	elseif string.match (file, "*-doc.tex$" ) then
+		content = string.gsub (
+			content,
+			"\\date{Version v%d%.%d",
+			"\\date{Version v" .. packageversion
+		)
+		return content
+	end
  return content
 end
